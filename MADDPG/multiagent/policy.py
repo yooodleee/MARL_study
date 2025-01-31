b32cc1d933a22b8de926f10ced9529d4a6aecba2
yooodleee <accia25@naver.com>
2025-02-01 01:30:28 +0900

    keyboard event callbacks

--- MADDPG/multiagent/policy.py
+++ MADDPG/multiagent/policy.py
@@ -37,8 +37,8 @@ class InteractivePolicy(Policy):
         self.move = [False for i in range(4)]
         self.comm = [False for i in range(env.world.dim_c)]
 
-        env.viewers[agent_index].window.on_key_press = self.key_press
-        env.viewers[agent_index].window.on_key_release = self.key_release
+        env.viewers[agent_index].window.on_key_press = self.key_press()
+        env.viewers[agent_index].window.on_key_release = self.key_release()
     
     def action(self, obs):
         """
@@ -62,4 +62,17 @@ class InteractivePolicy(Policy):
         
         return np.concatenate([u, np.zeros(self.env.world.dim_c)])
     
+    def key_press(self, k, mod):
+        """
+        Keyboard event callbacks.
+        """
+        if k == key.LEFT:   self.move[0] = True
+        if k == key.RIGHT:  self.move[1] = True
+        if k == key.UP:     self.move[2] = True
+        if k == key.DOWN:   self.move[3] = True
     
+    def key_release(self, k, mod):
+        if k == key.LEFT:   self.move[0] = False
+        if k == key.RIGHT:  self.move[1] = False
+        if k == key.UP:     self.move[2] = False
+        if k == key.DOWN:   self.move[3] = False
