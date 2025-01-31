616137784f5569612e5aaa3427abbcac436a04ef
yooodleee <accia25@naver.com>
2025-01-31 17:04:13 +0900

    add Entity

--- MADDPG/multiagent/core.py
+++ MADDPG/multiagent/core.py
@@ -30,3 +30,23 @@ class Action(object):
         self.c = None
 
 
+# properties and state of physical world entity
+class Entity(object):
+
+    def __init__(self):
+        self.name = ''              # name
+        self.size = 0.050           # properties
+        self.movable = False        # entity can move / be pushed
+        self.collide = True         # entity collides with others
+        self.density = 25.0         # material density (affects mass)
+        self.color = None           # color
+        self.max_speed = None       # max speed
+        self.accel = None           # accel
+        self.state = EntityState()  # state
+        self.initial_mass = 1.0     # mass
+    
+    @property
+    def mass(self):
+        return self.initial_mass
+
+
