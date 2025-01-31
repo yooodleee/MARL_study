011eaa09e63d2e89bb12856625bf2c48fba14e03
yooodleee <accia25@naver.com>
2025-02-01 01:09:05 +0900

    add MultiDiscrete

--- MADDPG/multiagent/multi_discrete.py
+++ MADDPG/multiagent/multi_discrete.py
@@ -12,5 +12,27 @@ from gym.spaces import prng
 
 class MultiDiscrete(gym.Space):
     """
+    - The multi-discrete action space consists of a series of discrete action spaces
+        with different parameters. 
+    - It can be adapted to both a Discrete action spae or a continuous (Box) action space. 
+    - It is usefult to represent game controllers or keyboards where each key can be represented 
+        as a discrete action space. 
+    - It is parameterized by passing an arrays containing [min, max] for each discrete action
+        space where the discrete action space can take any integers from `min` to `max` (both inclusive).
     
+    Note: A value of 0 always need to represent the NOOP action.
+    e.g. Nintendo Game Controoler
+    - Can be conceptualized as 3 discrete action spaces:
+        1) Arrow Keys: Discrete 5  - NOOP[0], UP[1], RIGHT[2], DOWN[3], LEFT[4]  - params: min: 0, max: 4
+        2) Button A:   Discrete 2  - NOOP[0], Pressed[1] - params: min: 0, max: 1
+        3) Button B:   Discrete 2  - NOOP[0], Pressed[1] - params: min: 0, max: 1
+    - Can be initialized as:
+        MultiDiscrete([ [0, 4], [0, 1], [0, 1] ])
+
     """
+    def __init__(self, array_of_param_array):
+        self.low = np.array(x[0] for x in array_of_param_array)
+        self.high = np.array(x[1] for x in array_of_param_array)
+        self.num_discrete_space = self.low.shape[0]
+    
+    
