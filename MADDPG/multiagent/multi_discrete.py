db92d7a7d767d96e15cede186b7c7422adaac8d9
yooodleee <accia25@naver.com>
2025-02-01 01:13:21 +0900

    returns a array with one sample

--- MADDPG/multiagent/multi_discrete.py
+++ MADDPG/multiagent/multi_discrete.py
@@ -35,4 +35,23 @@ class MultiDiscrete(gym.Space):
         self.high = np.array(x[1] for x in array_of_param_array)
         self.num_discrete_space = self.low.shape[0]
     
+    def sample(self):
+        """
+        Returns a array with one sample from each discrete action space.
+        
+        For each row:
+            round(random .* (max - min) + min, 0)
+
+        """
+        random_array = prng.np_random.rand(self.num_discrete_space)
+        return [
+            int(x)
+            for x in np.floor(
+                np.multiply(
+                    (self.high - self.low + 1.), random_array
+                )
+                + self.low
+            )
+        ]
+    
     
