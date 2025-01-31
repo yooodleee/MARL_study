d49b9d9edf049bc02529185ea52dabdbdeabf562
yooodleee <accia25@naver.com>
2025-01-31 19:19:07 +0900

    create multi_discrete.py

---
+++ MADDPG/multiagent/multi_discrete.py
@@ -0,0 +1,16 @@
+"""
+An old version of OpenAI Gym's multi_discrete.py. (Was getting affected by
+    Gym updates)
+(https://github.com/openai/gym/blob/1fb81d4e3fb780ccf77fec731287ba07da35eb84/gym/spaces/multi_discrete.py)
+
+"""
+
+import numpy as np
+import gym
+from gym.spaces import prng
+
+
+class MultiDiscrete(gym.Space):
+    """
+    
+    """
