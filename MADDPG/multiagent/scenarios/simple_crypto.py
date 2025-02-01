a4a3eed990b5327bfc3a5e0e47a3da0a839eab12
yooodleee <accia25@naver.com>
2025-02-02 01:35:59 +0900

    add Scenario
    make the world

--- MADDPG/multiagent/scenarios/simple_crypto.py
+++ MADDPG/multiagent/scenarios/simple_crypto.py
@@ -18,3 +18,36 @@ class CryptoAgent(Agent):
         self.key = None
     
 
+class Scenario(BaseScenario):
+
+    def make_world(self):
+        world = World()
+
+        # set any world properties first
+        num_agents = 3
+        num_adversaries = 1
+        num_landmarks = 2
+        world.dim_c = 4
+
+        # add agents
+        world.agents = [CryptoAgent() for i in range(num_agents)]
+        for i, agent in enumerate(world.agents):
+            agent.name = 'agent %d' % i
+            agent.collide = False
+            agent.adversary = True if i < num_adversaries else False
+            agent.speaker = True if i == 2 else False
+            agent.movable = False
+        
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(num_landmarks)]
+        for i, landmakr in enumerate(world.landmarks):
+            landmakr.name = 'landmark %d' % i
+            landmakr.collide = False
+            landmakr.movable = False
+        
+        # make initial conditions
+        self.reset_world(world)
+
+        return world
+    
+    
