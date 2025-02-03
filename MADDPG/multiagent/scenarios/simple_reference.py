a5362f8e58e922f331e7a3803492c26f59bef446
yooodleee <accia25@naver.com>
2025-02-03 10:20:20 +0900

    make_world
    set any world properties first
    add agents and landmarks
    make initial conidtions

--- MADDPG/multiagent/scenarios/simple_reference.py
+++ MADDPG/multiagent/scenarios/simple_reference.py
@@ -5,4 +5,30 @@ from multiagent.scenario import BaseScenario
 
 
 class Scenario(BaseScenario):
+
+    def make_world(self):
+        world = World()
+
+        # set any world properties first
+        world.dim_c = 10
+        world.collaborative = True  # whether agents share rewards
+
+        # add agents
+        world.agents = [Agent() for i in range(2)]
+        for i, agent in enumerate(world.agents):
+            agent.name = 'agent %d' % i
+            agent.collide = False
+        
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(3)]
+        for i, landmark in enumerate(world.landmarks):
+            landmark.name = 'landmark %d' % i
+            landmark.collide = False
+            landmark.movable = False
+        
+        # make initial conditions
+        self.reset_world(world)
+        return world
+    
+
     
