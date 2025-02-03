e9e5a34a4b85dcc77702b7f0e37f57611bfaeae9
yooodleee <accia25@naver.com>
2025-02-03 10:36:18 +0900

    make_world
    set any world properties first

--- MADDPG/multiagent/scenarios/simple_speaker_listener.py
+++ MADDPG/multiagent/scenarios/simple_speaker_listener.py
@@ -6,4 +6,38 @@ from multiagent.scenario import BaseScenario
 
 class Scenario(BaseScenario):
 
+    def make_world(self):
+        world = World()
+
+        # set any world properties first
+        world.dim_c = 3
+        num_landmarks = 3
+        world.collaborative = True
+
+        # add agents
+        world.agents = [Agent() for i in range(2)]
+        for i, agent in enumerate(world.agents):
+            agent.name = 'agent %d' % i
+            agent.collide = False
+            agent.size = 0.075
+        
+        # speaker
+        world.agents[0].movable = False
+
+        # listener
+        world.agents[1].silent = True
+
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(num_landmarks)]
+        for i, landmark in enumerate(world.landmarks):
+            landmark.name = 'landmark %d' % i
+            landmark.collide = False
+            landmark.movable = False
+            landmark.size = 0.04
+        
+        # make initial conditions
+        self.reset_world(world)
+        return world
+    
+
     
