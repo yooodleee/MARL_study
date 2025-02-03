a3eb0e60b66455533e083837bb69777c3e5266cc
yooodleee <accia25@naver.com>
2025-02-03 11:24:03 +0900

    make_world

--- MADDPG/multiagent/scenarios/simple_tag.py
+++ MADDPG/multiagent/scenarios/simple_tag.py
@@ -6,4 +6,41 @@ from multiagent.scenario import BaseScenario
 
 class Scenario(BaseScenario):
 
+    def make_world(self):
+        world = World()
+
+        # set any world properties first
+        world.dim_c = 2
+        num_good_agents = 1
+        num_adversaries = 3
+        num_agents = num_adversaries + num_good_agents
+        num_landmarks = 2
+
+        # add agents
+        world.agents = [Agent() for i in range(num_agents)]
+        for i, agent in enumerate(world.agents):
+            agent.name = 'agent %d' % i
+            agent.collide = True
+            agent.silent = True
+            agent.adversary = True if i < num_adversaries else False
+            agent.size = 0.075 if agent.adversary else 0.05
+            agent.accel = 3.0 if agent.adversary else 4.0
+
+            # agent.accel = 20.0 if agent.adversary else 25.0
+            agent.max_speed = 1.0 if agent.adversary else 1.3
+        
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(num_landmarks)]
+        for i, landmark in enumerate(world.landmarks):
+            landmark.name = 'landmark %d' % i
+            landmark.collide = True
+            landmark.movable = False
+            landmark.size = 0.2
+            landmark.boundary = False
+        
+        # make initial conditions
+        self.reset_world(world)
+        return world
+    
+
     
