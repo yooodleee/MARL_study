b3857a493e5c19a1c75e94dab2760e6d773a4d81
yooodleee <accia25@naver.com>
2025-02-03 12:06:41 +0900

    make_world
    set world properties first
    add agents, landmarks
    make initial conditions

--- MADDPG/multiagent/scenarios/simple_world_comm.py
+++ MADDPG/multiagent/scenarios/simple_world_comm.py
@@ -6,4 +6,64 @@ from multiagent.scenario import BaseScenario
 
 class Scenario(BaseScenario):
 
+    def make_world(self):
+        world = World()
+
+        # set any world properties first
+        world.dim_c = 4
+        # world.damping = 1
+        num_good_agents = 2
+        num_adversaries = 4
+        num_agents = num_adversaries + num_good_agents
+        num_landmarks = 1
+        num_food = 2
+        num_forests = 2
+        
+        # add agents
+        world.agents = [Agent() for i in range(num_agents)]
+        for i, agent in enumerate(world.agents):
+            agent.name = 'agent %d' % i
+            agent.leader = True if i == 0 else False
+            agent.silent = True if i > 0 else False
+            agent.adversary = True if i < num_adversaries else False
+            agent.size = 0.075 if agent.adversary else 0.045
+            agent.accel = 3.0 if agent.adversary else 4.0
+            # agent.accel = 20.0 if agent.adversary else 25.0
+            agent.max_speed = 1.0 if agent.adversary else 1.3
+        
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(num_landmarks)]
+        for i, landmark in enumerate(world.landmarks):
+            landmark.name = 'landmark %d' % i
+            landmark.collide = True
+            landmark.movable = False
+            landmark.size = 0.2
+            landmark.boundary = False
+        
+        world.food = [Landmark() for i in range(num_food)]
+        for i, landmark in enumerate(world.food):
+            landmark.name = 'food %d' % i
+            landmark.collide = False
+            landmark.movable = False
+            landmark.size = 0.03
+            landmark.bondary = False
+        
+        world.forests = [Landmark() for i in range(num_forests)]
+        for i, landmar in enumerate(world.forests):
+            landmark.name = 'forest %d' % i
+            landmark.collide = False
+            landmark.movable = False
+            landmark.size = 0.3
+            landmark.boundary = False
+        
+        world.landmarks += world.food
+        world.landmarks += world.forests
+        # world.landmarks += self.set_boundaries(world) 
+        # world boundaries now penalize with negative reward
+
+        # make initial conditions
+        self.reset_world(world)
+        return world
+    
+
     
