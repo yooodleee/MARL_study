d0eca531110f42a2acf1d6f7d02909498397ce7a
yooodleee <accia25@naver.com>
2025-02-03 10:41:24 +0900

    reset_world

--- MADDPG/multiagent/scenarios/simple_speaker_listener.py
+++ MADDPG/multiagent/scenarios/simple_speaker_listener.py
@@ -40,4 +40,38 @@ class Scenario(BaseScenario):
         return world
     
 
+    def reset_world(self, world):
+        
+        # assign goals to agents
+        for agent in world.agents:
+            agent.goal_a = None
+            agent.goal_b = None
+        
+        # want listener to go to the goal landmark
+        world.agents[0].goal_a = world.agents[1]
+        world.agents[1].goal_b = np.random.choice(world.landmarks)
+
+        # random properties for agents
+        for i, agent in enumerate(world.agents):
+            agent.color = np.array([0.25, 0.25, 0.25])
+        
+        # random properties for landmarks
+        world.landmarks[0].color = np.array([0.65, 0.15, 0.15])
+        world.landmarks[1].color = np.array([0.15, 0.65, 0.15])
+        world.landmarks[2].color = np.array([0.15, 0.15, 0.65])
+
+        # special colors for goals
+        world.agents[0].goal_a.color = world.agents[0].goal_b.color + np.array([0.45, 0.45, 0.45])
+
+        # set random initial states
+        for agent in world.agents:
+            agent.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
+            agent.state.p_vel = np.zeros(world.dim_p)
+            agent.state.c = np.zeros(world.dim_c)
+        
+        for i, landmark in enumerate(world.landmarks):
+            landmark.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
+            landmark.state.p_vel = np.zeros(world.dim_p)
+    
+
     
