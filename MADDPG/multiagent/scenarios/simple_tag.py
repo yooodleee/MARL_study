52d3742a15c308126292918d18f03815d9631f5c
yooodleee <accia25@naver.com>
2025-02-03 11:47:31 +0900

    observation
    get positions of all entities in this agent's reference frame

--- MADDPG/multiagent/scenarios/simple_tag.py
+++ MADDPG/multiagent/scenarios/simple_tag.py
@@ -197,4 +197,34 @@ class Scenario(BaseScenario):
         return rew
     
 
-    
+    def observation(self, agent, world):
+        """
+        Get positions of all entities in this agent's reference frame
+        """
+        entity_pos = []
+        for entity in world.landmarks:
+            if not entity.boundary:
+                entity_pos.append(entity.state.p_pos - agent.state.p_pos)
+        
+        # communication of all other agents
+        comm = []
+        other_pos = []
+        other_vel = []
+
+        for other in world.agents:
+            if other is agent:
+                continue
+
+            comm.append(other.state.c)
+            other_pos.append(other.state.p_pos - agent.state.p_pos)
+
+            if not other.adversary:
+                other_vel.append(other.state.p_vel)
+        
+        return np.concatenate(
+            [agent.state.p_vel] \
+            + [agent.state.p_pos] \
+            + entity_pos \
+            + other_pos \
+            + other_vel
+        )
