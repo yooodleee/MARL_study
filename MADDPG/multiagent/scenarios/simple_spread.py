54dab265a6fed6a0ade166fe89943f20e258a900
yooodleee <accia25@naver.com>
2025-02-03 11:04:33 +0900

    make_world
    set any world properties first

--- MADDPG/multiagent/scenarios/simple_spread.py
+++ MADDPG/multiagent/scenarios/simple_spread.py
@@ -6,4 +6,33 @@ from multiagent.scenario import BaseScenario
 
 class Scenario(BaseScenario):
 
+    def make_world(self):
+        world = World()
+
+        # set any world properties first
+        world.dim_c = 2
+        num_agents = 3
+        num_landmarks = 3
+        world.collaborative = True
+
+        # add agents
+        world.agents = [Agent() for i in range(num_agents)]
+        for i, agent in enumerate(world.agents):
+            agent.name = 'agent %d' % i
+            agent.collide = True
+            agent.silent = True
+            agent.size = 0.15
+        
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(num_landmarks)]
+        for i, landmark in enumerate(world.landmarks):
+            landmark.name = 'landmark %d' % i
+            landmark.collide = False
+            landmark.movable = False
+        
+        # make initial conditions
+        self.reset_world(world)
+        return world
+    
+
     
