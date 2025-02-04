efc45f618899b52e13a185c695a996506528df10
yooodleee <accia25@naver.com>
2025-02-04 13:56:47 +0900

    create __init__.py

---
+++ CTDE/CADP-PG/onpolicy/__init__.py
@@ -0,0 +1,14 @@
+from onpolicy import algos, envs, runner, scripts, utils, config
+
+
+__version__ = "0.1.0"
+
+
+__all__ = [
+    "algos",
+    "envs",
+    "runner",
+    "scripts",
+    "utils",
+    "config",
+]
