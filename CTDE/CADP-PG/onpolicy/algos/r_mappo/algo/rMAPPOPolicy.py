2a63b734ac8a4f25ab37537063ab1ce27d7dec8d
yooodleee <accia25@naver.com>
2025-02-04 16:45:39 +0900

    act
    compute actions using the given inputs.

--- CTDE/CADP-PG/onpolicy/algos/r_mappo/algo/rMAPPOPolicy.py
+++ CTDE/CADP-PG/onpolicy/algos/r_mappo/algo/rMAPPOPolicy.py
@@ -271,4 +271,39 @@ class R_MAPPOPolicy:
         return values, action_log_probs, dist_entropy
     
 
-    
+    def act(
+            self,
+            obs,
+            rnn_states_actor,
+            masks,
+            available_actions=None,
+            deterministic=False):
+        
+        """
+        Compute actions using the given inputs.
+
+
+        Params
+        -----------
+            obs: (np.ndarray)
+                local agent inputs to the actor.
+            rnn_states_actor: (np.ndarray)
+                if actor is RNN, RNN states for actor.
+            masks: (np.ndarray)
+                denotes points at which RNN states should be reset.
+            available_actions: (np.ndarray)
+                denotes which actions are available to agent (if None,
+                all actions available).
+            deterministic: (bool)
+                whether the action should be mode of distribution or
+                should be sampled. 
+        """
+        actions, _, rnn_states_actor = self.actor(
+            obs,
+            rnn_states_actor,
+            masks,
+            available_actions,
+            deterministic,
+        )
+
+        return actions, rnn_states_actor
