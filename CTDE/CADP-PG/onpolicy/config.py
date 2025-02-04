df61b94ae92ffb6f94e8d58c581f62ce391c9273
yooodleee <accia25@naver.com>
2025-02-04 15:06:08 +0900

    code

--- CTDE/CADP-PG/onpolicy/config.py
+++ CTDE/CADP-PG/onpolicy/config.py
@@ -411,3 +411,216 @@ def get_config():
         default=10,
         help="Time length of chunks used to train a recurrent_policy",
     )
+
+
+    # optimizer params
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=5e-4,
+        help='learning rate (default: 5e-4)',
+    )
+    parser.add_argument(
+        "--critic_lr",
+        type=float,
+        default=5e-4,
+        help='critic learning rate (default: 5e-4)',
+    )
+    parser.add_argument(
+        "--opti_eps",
+        type=float,
+        default=1e-5,
+        help='RMSprop optimizer epsilon (default: 1e-5)',
+    )
+    parser.add_argument(
+        "--weight_decay",
+        type=float,
+        default=0,
+    )
+
+
+    # ppo params
+    parser.add_argument(
+        "--ppo_epoch",
+        type=int,
+        default=5,
+        help='number of ppo epochs (default: 5)',
+    )
+    parser.add_argument(
+        "--use_clipped_value_loss",
+        action='store_false',
+        default=True,
+        help="by default, clip loss value. If set, do not clip loss value.",
+    )
+    parser.add_argument(
+        "--clip_param",
+        type=float,
+        default=0.2,
+        help='ppo clip parameter (default: 0.2)',
+    )
+    parser.add_argument(
+        "--num_mini_batch",
+        type=int,
+        default=1,
+        help='number of batches for ppo (default: 1)',
+    )
+    parser.add_argument(
+        "--entropy_coef", 
+        type=float, 
+        default=0.01,
+        help='entropy term coefficient (default: 0.01)',
+    )
+    parser.add_argument(
+        "--value_loss_coef", 
+        type=float,
+        default=1, 
+        help='value loss coefficient (default: 0.5)',
+    )
+    parser.add_argument(
+        "--use_max_grad_norm",
+        action='store_false', 
+        default=True, 
+        help="by default, use max norm of gradients. If set, do not use."
+    )
+    parser.add_argument(
+        "--max_grad_norm", 
+        type=float, 
+        default=10.0,
+        help='max norm of gradients (default: 0.5)'
+    )
+    parser.add_argument(
+        "--use_gae", 
+        action='store_false',
+        default=True, 
+        help='use generalized advantage estimation'
+    )
+    parser.add_argument(
+        "--gamma", 
+        type=float, 
+        default=0.99,
+        help='discount factor for rewards (default: 0.99)'
+    )
+    parser.add_argument(
+        "--gae_lambda", 
+        type=float, 
+        default=0.95,
+        help='gae lambda parameter (default: 0.95)'
+    )
+    parser.add_argument(
+        "--use_proper_time_limits", 
+        action='store_true',
+        default=False, 
+        help='compute returns taking into account time limits'
+    )
+    parser.add_argument(
+        "--use_huber_loss", 
+        action='store_false', 
+        default=True, 
+        help="by default, use huber loss. If set, do not use huber loss."
+    )
+    parser.add_argument(
+        "--use_value_active_masks",
+        action='store_false', 
+        default=True, 
+        help="by default True, whether to mask useless data in value loss."
+    )
+    parser.add_argument(
+        "--use_policy_active_masks",
+        action='store_false', 
+        default=True, 
+        help="by default True, whether to mask useless data in policy loss."
+    )
+    parser.add_argument(
+        "--huber_delta", 
+        type=float, 
+        default=10.0, 
+        help=" coefficience of huber loss."
+    )
+
+
+    # run parameters
+    parser.add_argument(
+        "--use_linear_lr_decay", 
+        action='store_true',
+        default=False, 
+        help='use a linear schedule on the learning rate'
+    )
+
+
+    # save parameters
+    parser.add_argument(
+        "--save_interval", 
+        type=int, 
+        default=1, 
+        help="time duration between contiunous twice models saving."
+    )
+
+
+    # log parameters
+    parser.add_argument(
+        "--log_interval", 
+        type=int, 
+        default=5, 
+        help="time duration between contiunous twice log printing."
+    )
+
+
+    # eval parameters
+    parser.add_argument(
+        "--use_eval", 
+        action='store_true', 
+        default=False, 
+        help="by default, do not start evaluation. If set`, start evaluation alongside with training."
+    )
+    parser.add_argument(
+        "--eval_interval", 
+        type=int, 
+        default=25, 
+        help="time duration between contiunous twice evaluation progress."
+    )
+    parser.add_argument(
+        "--eval_episodes", 
+        type=int, 
+        default=32, 
+        help="number of episodes of a single evaluation."
+    )
+
+
+    # render parameters
+    parser.add_argument(
+        "--save_gifs", 
+        action='store_true', 
+        default=False, 
+        help="by default, do not save render video. If set, save video."
+    )
+    parser.add_argument(
+        "--use_render", 
+        action='store_true', 
+        default=False, 
+        help="by default, do not render the env during training. \
+            If set, start render. Note: something, the environment \
+            has internal render process which is not controlled by this hyperparam."
+    )
+    parser.add_argument(
+        "--render_episodes", 
+        type=int, 
+        default=5, 
+        help="the number of episodes to render a given env"
+    )
+    parser.add_argument(
+        "--ifi", 
+        type=float, 
+        default=0.1, 
+        help="the play interval of each rendered image in saved video."
+    )
+
+
+    # pretrained parameters
+    parser.add_argument(
+        "--model_dir", 
+        type=str, 
+        default=None, 
+        help="by default None. set the path to pretrained model."
+    )
+
+    return parser
