c9448933d37fca44d16329d9b96192e975f78619
yooodleee <accia25@naver.com>
2025-02-05 17:20:00 +0900

    add R_Critic
    critic network class for MAPPO

--- CTDE/CADP-PG/onpolicy/algos/r_mappo/algo/r_actor_critic_cadp.py
+++ CTDE/CADP-PG/onpolicy/algos/r_mappo/algo/r_actor_critic_cadp.py
@@ -330,3 +330,111 @@ class R_Actor(nn.Module):
     
 
 
+class R_Critic(nn.Module):
+    """
+    Critic network class for MAPPO. 
+    Outputs value function predictions given centralized input (MAPPO) 
+        or local observations (IPPO).
+
+
+    Params
+    ---------
+        args: (argparse.Namespace) 
+            args containing relevant model info.
+        cent_obs_space: (gym.Space) 
+            (centralized) observation space.
+        device: (torch.device) 
+            specifies the device to run on (cpu/gpu).
+    """
+
+    def __init__(
+            self, 
+            args, 
+            cent_obs_space, 
+            device=torch.device("cpu")):
+        
+        super(R_Critic, self).__init__()
+        self.hidden_size = args.hidden_size
+        self._use_orthogonal = args.use_orthogonal
+        self._use_naive_recurrent_policy = args.use_naive_recurrent_policy
+        self._use_recurrent_policy = args.use_recurrent_policy
+        self._recurrent_N = args.recurrent_N
+        self._use_popart = args.use_popart
+        self.tpdv = dict(dtype=torch.float32, device=device)
+
+        init_method = [
+            nn.init.xavier_uniform_, 
+            nn.init.orthogonal_
+        ][self._use_orthogonal]
+
+        cent_obs_shape = get_shape_from_obs_space(cent_obs_space)
+        base = CNNBase if len(cent_obs_shape) == 3 else MLPBase
+        self.base = base(args, cent_obs_shape)
+
+        if self._use_naive_recurrent_policy or self._use_recurrent_policy:
+            self.rnn = RNNLayer(
+                self.hidden_size, 
+                self.hidden_size, 
+                self._recurrent_N, 
+                self._use_orthogonal
+            )
+
+        def init_(m):
+            return init(
+                m, 
+                init_method, 
+                lambda x: nn.init.constant_(x, 0)
+            )
+
+        if self._use_popart:
+            self.v_out = init_(
+                PopArt(self.hidden_size, 1, device=device)
+            )
+        else:
+            self.v_out = init_(nn.Linear(self.hidden_size, 1))
+
+        self.to(device)
+
+    def forward(
+            self, 
+            cent_obs, 
+            rnn_states, 
+            masks):
+        
+        """
+        Compute actions from the given inputs.
+
+
+        Params
+        ---------
+            cent_obs: (np.ndarray / torch.Tensor) 
+                observation inputs into network.
+            rnn_states: (np.ndarray / torch.Tensor) 
+                if RNN network, hidden states for RNN.
+            masks: (np.ndarray / torch.Tensor) 
+                mask tensor denoting if RNN states should be reinitialized
+                to zeros.
+
+        Returns
+        ----------
+            values: (torch.Tensor) 
+                value function predictions.
+            rnn_states: (torch.Tensor) 
+                updated RNN hidden states.
+        """
+
+        cent_obs = check(cent_obs).to(**self.tpdv)
+        rnn_states = check(rnn_states).to(**self.tpdv)
+        masks = check(masks).to(**self.tpdv)
+
+        critic_features = self.base(cent_obs)
+
+        if self._use_naive_recurrent_policy or self._use_recurrent_policy:
+            critic_features, rnn_states = self.rnn(
+                critic_features, 
+                rnn_states, 
+                masks
+            )
+        values = self.v_out(critic_features)
+
+        return values, rnn_states
