8e7e88042d7591d038b387bab7920276c18a79be
yooodleee <accia25@naver.com>
2025-02-05 16:36:07 +0900

    init
    add R_Actor

--- CTDE/CADP-PG/onpolicy/algos/r_mappo/algo/r_actor_critic_cadp.py
+++ CTDE/CADP-PG/onpolicy/algos/r_mappo/algo/r_actor_critic_cadp.py
@@ -81,3 +81,75 @@ class SelfAttention(nn.Module):
     
 
 
+class R_Actor(nn.Module):
+    """
+    Actor network class for MAPPO.
+    Outputs actions given observations.
+
+
+    Params
+    -----------
+        args: (argparse.Namespace)
+            arguments containing relevant model info.
+        obs_space: (gym.Space)
+            observation space.
+        action_space: (gym.Space)
+            action space.
+        device: (torch.device)
+            specifies the device to run on (cpu/gpu).
+    """
+
+    def __init__(
+            self,
+            args,
+            obs_space,
+            action_space,
+            device=torch.device("cpu")):
+        
+        super(R_Actor, self).__init__()
+        self.n_rollout_threads = args.n_rollout_threads
+        self.num_agents = args.num_agents
+
+        self.hidden_size = args.hidden_size
+
+        self._gain = args.gain
+        self._use_orthogonal = args.use_orthogonal
+        self._use_policy_active_masks = args.use_policy_active_masks
+        self._use_naive_recurrent_policy = args.use_naive_recurrent_policy
+        self._use_recurrent_policy = args.use_recurrent_policy
+        self._recurrent_N = args.recurrent_N
+        self.tpdv = dict(dtype=torch.float32, device=device)
+
+        obs_shape = get_shape_from_obs_space(obs_space)
+        base = CNNBase if len(obs_shape) == 3 else MLPBase
+        self.base = base(args, obs_shape)
+
+        if self._use_naive_recurrent_policy or self._use_recurrent_policy:
+            self.rnn = RNNLayer(
+                self.hidden_size,
+                self.hidden_size,
+                self._recurrent_N,
+                self._use_orthogonal,
+            )
+
+        self.obs_dim = obs_shape[0]
+        self.att = SelfAttention(self.obs_dim, 4, 32)
+        
+        self.fc1 = nn.Linear(4 * 32, self.hidden_size)
+        self.fc2 = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        # self.fc2 = nn.Linear(self.hidden_size, self.hidden_size)
+        
+        self.use_att_v = False
+
+        self.act = ACTLayer(
+            action_space,
+            self.hidden_size,
+            self._use_orthogonal,
+            self._gain,
+        )
+
+        self.to(device)
+
+
+    
+    
