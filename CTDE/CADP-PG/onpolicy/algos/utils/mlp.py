74da616f3f9104694c30e65943155bfc97f8bc28
yooodleee <accia25@naver.com>
2025-02-06 10:44:47 +0900

    init
    add MLPBase

--- CTDE/CADP-PG/onpolicy/algos/utils/mlp.py
+++ CTDE/CADP-PG/onpolicy/algos/utils/mlp.py
@@ -63,3 +63,37 @@ class MLPLayer(nn.Module):
     
 
 
+class MLPBase(nn.Module):
+
+    def __init__(
+            self,
+            args,
+            obs_shape,
+            cat_self=True,
+            attn_internal=False):
+        
+        super(MLPBase, self).__init__()
+
+        self._use_feature_normalization = args.use_feature_normalization
+        self._use_orthogonal = args.use_orthogonal
+        self._use_ReLU = args.use_ReLU
+        self._stacked_frames = args.use_ReLU
+        self._layer_N = args.layer_N
+        self.hidden_size = args.hidden_size
+
+        obs_dim = obs_shape[0]
+
+        if self._use_feature_normalization:
+            self.feature_norm = nn.LayerNorm(obs_dim)
+        
+
+        self.mlp = MLPLayer(
+            obs_dim,
+            self.hidden_size,
+            self._layer_N,
+            self._use_orthogonal,
+            self._use_ReLU
+        )
+
+    
+    
