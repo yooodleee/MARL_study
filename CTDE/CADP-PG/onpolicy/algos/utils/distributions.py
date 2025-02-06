1cfb5471e1534fc722591b8cd8415ae91f064eee
yooodleee <accia25@naver.com>
2025-02-06 10:35:24 +0900

    forward

--- CTDE/CADP-PG/onpolicy/algos/utils/distributions.py
+++ CTDE/CADP-PG/onpolicy/algos/utils/distributions.py
@@ -189,4 +189,10 @@ class AddBias(nn.Module):
         self._bias = nn.Parameter(bias.unsqueeze(1))
     
 
-    
+    def forward(self, x):
+        if x.dim() == 2:
+            bias = self._bias.t().view(1, -1)
+        else:
+            bias = self._bias.t().view(1, -1, 1, 1)
+        
+        return x + bias
