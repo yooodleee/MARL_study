0fe4790557e1955fb1abb6e02bd09e9f35fc4e15
yooodleee <accia25@naver.com>
2025-02-06 11:27:01 +0900

    init

---
+++ CTDE/CADP-PG/onpolicy/algos/utils/util.py
@@ -0,0 +1,21 @@
+
+import copy
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+
+
+def init(
+        module,
+        weight_init,
+        bias_init,
+        gain=1):
+    
+    weight_init(module.weight.data, gain=gain)
+    bias_init(module.bias.data)
+
+    return module
+
+
