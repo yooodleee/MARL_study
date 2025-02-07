c756631a84f90c2f58d400a187547393556bc267
yooodleee <accia25@naver.com>
2025-02-07 10:48:02 +0900

    add ShareSubprocVecEnv

--- CTDE/CADP-PG/onpolicy/envs/env_wrappers.py
+++ CTDE/CADP-PG/onpolicy/envs/env_wrappers.py
@@ -479,7 +479,7 @@ def shareworker(
                     ob, s_ob, reward, done, info, available_actions
                 )
             )
-            
+
         elif cmd == 'reset':
             ob, s_ob, available_actions = env.reset()
             remote.send((ob, s_ob, available_actions))
@@ -516,3 +516,114 @@ def shareworker(
         
         else:
             raise NotImplementedError
+
+
+
+
+class ShareSubprocVecEnv(ShareVecEnv):
+
+    def __init__(
+            self,
+            env_fns,
+            spaces=None):
+        
+        """
+        envs:
+            list of gym envs to run in subprocesses.
+        """
+        self.waiting = False
+        self.closed = False
+
+        nenvs = len(env_fns)
+
+        self.remotes, self.work_remotes = zip(
+            *[Pipe() for _ in range(nenvs)]
+        )
+        self.ps = [
+            Process(
+                target=shareworker,
+                args=(work_remote, remote, CloudpickleWrapper(env_fn))
+            )
+            for (work_remote, remote, env_fn)
+            in zip(self.work_remotes, self.remotes, env_fns)
+        ]
+
+        for p in self.ps:
+            p.daemon = True # if the main process crashes, should not things to hang
+            p.start()
+        
+        for remote in self.work_remotes:
+            remote.close()
+        
+        self.remotes[0].send(('get_spaces', None))
+
+        observation_space, \
+        share_observation_space, \
+        action_space = self.remotes[0].recv()
+
+        ShareVecEnv.__init__(
+            self,
+            len(env_fns),
+            observation_space,
+            share_observation_space,
+            action_space
+        )
+
+
+    def step_async(self, actions):
+        for remote, action in zip(self.remotes, actions):
+            remote.send(('step', action))
+        
+        self.waiting = True
+    
+
+    def step_wait(self):
+        results = [remote.recv() for remote in self.remotes]
+        self.waiting = False
+
+        obs, share_obs, rews, dones, \
+        infos, available_actions = zip(*results)
+
+        return np.stack(obs), np.stack(share_obs), np.stack(rews), \
+            np.stack(dones), infos, np.stack(available_actions)
+    
+
+    def reset(self):
+        for remote in self.remotes:
+            remote.send(('reset', None))
+        
+        results = [remote.recv() for remote in self.remotes]
+
+        obs, share_obs, avialable_actions = zip(*results)
+
+        return np.stack(obs), np.stack(share_obs), \
+            np.stack(avialable_actions)
+    
+
+    def reset_task(self):
+        for remote in self.remotes:
+            remote.send(('reset_task', None))
+        
+        return np.stack(
+            [remote.recv() for remote in self.remotes]
+        )
+    
+
+    def close(self):
+        if self.closed:
+            return
+        
+        if self.waiting:
+            for remote in self.remotes:
+                remote.recv()
+        
+        for remote in self.remotes:
+            remote.send(('close', None))
+        
+        for p in self.ps:
+            p.join()
+        
+        self.closed = True
+
+
+
