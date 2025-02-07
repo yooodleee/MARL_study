1d707eeb2cde374aab090291170da8c69defe97a
yooodleee <accia25@naver.com>
2025-02-07 11:09:15 +0900

    add ChooseSimpleSubprocVecEnv

--- CTDE/CADP-PG/onpolicy/envs/env_wrappers.py
+++ CTDE/CADP-PG/onpolicy/envs/env_wrappers.py
@@ -678,3 +678,117 @@ def choosesimpleworker(
 
 
 
+class ChooseSimpleSubprocVecEnv(ShareVecEnv):
+
+    def __init__(
+            self,
+            env_fns,
+            spaces=None):
+        
+        """
+        envs:
+            list of gym envs to run in subprocesses.
+        """
+
+        self.waiting = False
+        self.closed = False
+
+        nenvs = len(env_fns)
+
+        self.remotes, self.work_remotes = zip(
+            *[Pipe() for _ in range(nenvs)]
+        )
+        self.ps = [
+            Process(
+                target=choosesimpleworker,
+                args=(work_remote, remote, CloudpickleWrapper(env_fn))
+            )
+            for (work_remote, remote, env_fn)
+            in zip(self.work_remotes, self.remotes, env_fns)
+        ]
+
+        for p in self.ps:
+            p.daemon = True # if the main process crashes, should not cause things to hang
+            p.start()
+        
+        for remote in self.work_remotes:
+            remote.close()
+        
+        self.remotes[0].send(('get_spaces', None))
+
+        observation_space, \
+        share_observation_space, \
+        action_space = self.remotes[0].recv()
+
+        ShareVecEnv.__init__(
+            self,
+            len(env_fns),
+            observation_space,
+            share_observation_space,
+            action_space
+        )
+
+
+    def step_async(self, actions):
+        for remote, action in zip(self.remotes, actions):
+            remote.send(('step', action))
+        
+        self.waiting = True
+
+    
+    def step_wait(self):
+        results = [remote.recv() for remote in self.remotes]
+        self.waiting = False
+
+        obs, rews, dones, infos = zip(*results)
+
+        return np.stack(obs), np.stack(rews), np.stack(dones), infos
+    
+
+    def reset(self, reset_choose):
+        for remote, choose in zip(self.remotes, reset_choose):
+            remote.send(('reset', choose))
+        
+        obs = [remote.recv() for remote in self.remotes]
+
+        return np.stack(obs)
+    
+
+    def render(self, mode='rgb_array'):
+        for remote in self.remotes:
+            remote.send(('render', mode))
+        
+        if mode == 'rgb_array':
+            frame = [remote.recv() for remote in self.remotes]
+
+            return np.stack(frame)
+    
+
+    def reset_task(self):
+        for remote in self.remotes:
+            remote.send(('reset_task', None))
+        
+        return np.stack(
+            [remote.recv() for remote in self.remotes]
+        )
+    
+
+    def close(self):
+        if self.closed:
+            return
+        
+        if self.waiting:
+            for remote in self.remotes:
+                remote.recv()
+        
+        for remote in self.remotes:
+            remote.send(('close', None))
+        
+        for p in self.ps:
+            p.join()
+        
+        self.close = True
+    
+
+
+
