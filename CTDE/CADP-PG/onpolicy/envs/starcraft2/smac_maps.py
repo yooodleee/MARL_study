24f45c052bae1b3e394c7b2e276158af9c4bc891
yooodleee <accia25@naver.com>
2025-02-09 00:15:04 +0900

    add SMACMap

---
+++ CTDE/CADP-PG/onpolicy/envs/starcraft2/smac_maps.py
@@ -0,0 +1,17 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from pysc2.maps import lib
+
+
+
+class SMACMap(lib.Map):
+    directory = "SMAC_Maps"
+    download = "https://github.com/oxwhirl/smac#smac-maps"
+    palyers = 2
+    step_mul = 8
+    game_steps_per_episode = 0
+
+
