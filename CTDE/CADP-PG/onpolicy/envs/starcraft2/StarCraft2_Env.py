cde99cb1b8d3e7d6da094ebe2cd8ee6e6cf2f853
yooodleee <accia25@naver.com>
2025-02-08 20:19:23 +0900

    get_state_agent
    returns obs for agent_id.

--- CTDE/CADP-PG/onpolicy/envs/starcraft2/StarCraft2_Env.py
+++ CTDE/CADP-PG/onpolicy/envs/starcraft2/StarCraft2_Env.py
@@ -1865,4 +1865,163 @@ class StarCraft2Env(MultiAgentEnv):
     
 
 
-    
+    def get_state_agent(self, agent_id):
+        """
+        Returns obs for agent_id.
+        The obs is composed of:
+
+            - agent movement features (where it can move eto, height info and pathing grid).
+            - enemy features (available_to_attack, health, relative_x, relative_y, shield,
+                unit_type).
+            - ally features (visible, distance, relative_x, relative_y, shield, unit_type).
+            - agent unit features (health, shield, unit_type).
+
+
+        All of this info is flattened and concatenated into a list, in the aforementioned
+            order. To know the sizes of each of the features inside the final list of features,
+            take a look at the functions ``get_obs_move_feats_size()``.
+            ``get_obs_enemy_feats_size()``, ``get_obs_ally_feats_size()`` and
+            ``get_obs_own_feats_size()``.
+
+
+        The size of obs vec may vary, depending on the env configuration and type of units present
+            in the map. For instance, non-Porotoss units will not have shields, movement features
+            may or may not include terrain height and pathing grid, unit_type is not included if there
+            is only one type of unit in the map etc.
+
+
+        NOTE: Agents should have access only to their local obs during decentralized execution.
+        """
+
+        if self.obs_instead_of_state:
+            obs_concat = np.concatenate(self.get_obs(), axis=0).astype(np.float32)
+
+            return obs_concat
+        
+        unit = self.get_unit_by_id(agent_id)
+
+
+        move_feats_dim = self.get_obs_move_feats_size()
+        enemy_feats_dim = self.get_state_enemy_feats_size()
+        ally_feats_dim = self.get_state_ally_feats_size()
+        own_feats_dim = self.get-state_own_feats_size()
+
+        
+        move_feats = np.zeros(move_feats_dim, dtype=np.float32)
+        enemy_feats = np.zeros(enemy_feats_dim, dtype=np.float32)
+        ally_feats = np.zeros(ally_feats_dim, dtype=np.float32)
+        own_feats = np.zeros(own_feats_dim, dtype=np.float32)
+        agent_id_feats = np.zeros(self.n_agents, dtype=np.float32)
+
+
+        center_x = self.map_x / 2
+        center_y = self.map_y / 2
+
+
+        if (self.use_mustalive and unit.health > 0) \
+        or (not self.use_mustalive):    # otherwise dead, return all zeros
+            
+            x = unit.pos.x
+            y = unit.pos.y
+            sight_range = self.unit_sight_range(agent_id)
+
+
+            if sight_range == 0:
+                sight_range = 1
+            
+
+            # Movement features
+            avail_actions = self.get_avail_agent_actions(agent_id)
+            for m in range(self.n_actions_move):
+                move_feats[m] = avail_actions[m + 2]
+            
+
+            ind = self.n_actions_move
+
+
+            if self.state_pathing_grid:
+                move_feats[ind: ind + self.n_obs_pathing] = self.get_surrounding_pathing(unit)
+                ind += self.n_obs_pathing
+            
+
+            if self.state_terrian_height:
+                move_feats[ind:] = self.get_surrounding_height(unit)
+            
+
+            # Enemy features
+            for e_id, e_unit in self.enemies.items():
+                e_x = e_unit.pos.x
+                e_y = e_unit.pos.y
+                dist = self.distance(x, y, e_x, e_y)
+
+
+                # visible and alive.
+                if e_unit.health > 0:
+                    # Sight range > shoot range
+                    if unit.health > 0:
+                        enemy_feats[e_id, 0] = avail_actions[self.n_actions_no_attack + e_id] # available
+                        enemy_feats[e_id, 1] = dist / sight_range   # distance
+                        enemy_feats[e_id, 2] = (e_x - x) / sight_range # relative X
+                        enemy_feats[e_id, 3] = (e_y - y) / sight_range # relative Y
+
+                        if dist < sight_range:
+                            enemy_feats[e_id, 4] = 1 # visible
+                    
+                    ind = 5
+                    if self.obs_all_health:
+                        enemy_feats[e_id, ind] = (e_unit.health / e_unit.health_max) # health
+                        ind += 1
+
+
+                        if self.shield_bits_enemy > 0:
+                            max_shield = self.unit_max_shield(e_unit)
+                            enemy_feats[e_id, ind] = (e_unit.shield / max_shield) # shield
+                            ind += 1
+                    
+
+                    if self.unit_type_bits > 0:
+                        type_id = self.get_unit_type_id(e_unit, False)
+                        enemy_feats[e_id, ind + type_id] = 1 # unit type
+                        ind += self.unit_type_bits
+                    
+
+                    if self.add_center_xy:
+                        enemy_feats[e_id, ind] = (e_x - center_x) / self.max_distance_x # center X
+                        enemy_feats[e_id, ind + 1] = (e_y - center_y) / self.max_distance_y # cetner Y
+            
+
+            # Ally features
+            al_ids = [
+                al_id for al_id in range(self.n_agents)
+                if al_id != agent_id
+            ]
+            for i, al_id in enumerate(al_ids):
+
+                al_unit = self.get_unit_by_id(al_id)
+                al_x = al_unit.pos.x
+                al_y = al_unit.pos.y
+                dist = self.distance(x, y, al_x, al_y)
+                max_cd = self.unit_max_cooldown(al_unit)
+
+
+                if al_unit.health > 0:  # visible and alive.
+                    if unit.health > 0:
+                        if dist < sight_range:
+                            ally_feats[i, 0] = 1 # visible
+                        
+                        ally_feats[i, 1] = dist / sight_range # distance
+                        ally_feats[i, 2] = (al_x - x) / sight_range # relative X
+                        ally_feats[i, 3] = (al_y - y) / sight_range # relative Y
+                    
+
+                    if (
+                        self.map_type == "MMM"
+                        and al_unit.unit_type == self.medivac_id
+                    ):
+                        ally_feats[i, 4] = al_unit.energy / max_cd # energy
+                    else:
+                        ally_feats[i, 4] = (al_unit.weapon_cooldown / max_cd) # cooldown
+                    
+
+                    ind = 5
+                    
