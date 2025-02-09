68555a2c72fe8a469ac78294ccb14edb4c947a17
yooodleee <accia25@naver.com>
2025-02-09 19:12:52 +0900

    compute
    calc returns for the collected data.

--- CTDE/CADP-PG/onpolicy/runner/shared/base_runner.py
+++ CTDE/CADP-PG/onpolicy/runner/shared/base_runner.py
@@ -160,4 +160,23 @@ class Runner(object):
         raise NotImplementedError
     
 
+    @torch.no_grad()
+    def compute(self):
+        """
+        Calculate returns for the collected data.
+        """
+        self.trainer.prep_rollout()
+        next_values = self.trainer.policy.get_values(
+            np.concatenate(self.buffer.share_obs[-1]),
+            np.concatenate(self.buffer.rnn_states_critic[-1]),
+            np.concatenate(self.buffer.masks[-1]),
+        )
+        next_values = np.array(
+            np.split(_t2n(next_values), self.n_rollout_threads)
+        )
+        self.buffer.compute_returns(
+            next_values, self.trainer.value_normalizer
+        )
+    
+
     
