eaf1a27632a638fcf3589a21863c423816e84d68
yooodleee <accia25@naver.com>
2025-02-09 19:08:01 +0900

    init
    add Runner

--- CTDE/CADP-PG/onpolicy/runner/shared/base_runner.py
+++ CTDE/CADP-PG/onpolicy/runner/shared/base_runner.py
@@ -17,3 +17,114 @@ def _t2n(x):
 
 
 
+class Runner(object):
+    """
+    Base class for training recurrent policies.
+
+
+    Param
+    --------------
+        config: (dict)
+            Config dict containing params for training.
+    """
+
+    def __init__(self, config):
+
+        self.all_args = config['all_args']
+        self.envs = config['envs']
+        self.eval_envs = config['eval_envs']
+        self.device = config['device']
+        self.num_agents = config['num_agents']
+
+        if config.__contains__("render_envs"):
+            self.render_envs = config['render_envs']
+        
+
+        self.all_args.num_agents = config['num_agents']
+
+
+        # Params
+        self.env_name = self.all_args.env_name
+        self.algorithm_name = self.all_args.algorithm_name
+        self.experiment_name = self.all_args.experiment_name
+        self.use_centralized_V = self.all_args.use_centralized_V
+        self.use_obs_instead_of_state = self.all_args.use_obs_instead_of_state
+        self.num_env_steps = self.all_args.num_env_steps
+        self.episode_length = self.all_args.episode_length
+        self.n_rollout_threads = self.all_args.n_rollout_threads
+        self.n_eval_rollout_threads = self.all_args.n_eval_rollout_threads
+        self.use_linear_lr_decay = self.all_args.use_linear_lr_decay
+        self.hidden_size = self.all_args.hidden_size
+        self.use_wandb = self.all_args.use_wandb
+        self.use_render = self.all_args.use_render
+        self.recurrent_N = self.all_args.recurrent_N
+
+
+        # Interval
+        self.save_interval = self.all_args.save_interval
+        self.use_eval = self.all_args.use_eval
+        self.eval_interval = self.all_args.eval_interval
+        self.log_interval = self.all_args.log_interval
+
+
+        # Dir
+        self.model_dir = self.all_args.model_dir
+
+
+        if self.use_wandb:
+            self.save_dir = str(wandb.run.dir)
+            self.run_dir = str(wandb.run.dir)
+        
+        else:
+            self.run_dir = config["run_dir"]
+            self.log_dir = str(self.run_dir / 'logs')
+
+            if not os.path.exists(self.log_dir):
+                os.makedirs(self.log_dir)
+            
+            self.writter = SummaryWriter(self.log_dir)
+            self.save_dir = str(self.run_dir / 'models')
+
+            if not os.path.exists(self.save_dir):
+                os.makedirs(self.save_dir)
+        
+
+        from onpolicy.algos.r_mappo.r_mappo import R_MAPPO as TrainAlgo
+        from onpolicy.algos.r_mappo.algo.rMAPPOPolicy import R_MAPPOPolicy as Policy
+
+
+        share_observation_space = self.envs.share_observation_space[0] \
+            if self.use_centralized_V else self.envs.observation_space[0]
+
+        # Policy network
+        self.policy = Policy(
+            self.all_args,
+            self.envs.observation_space[0],
+            share_observation_space,
+            self.envs.action_space[0],
+            device=self.device,
+        )
+
+
+        # Algorithm
+        self.trainer = TrainAlgo(
+            self.all_args, self.policy, device=self.device
+        )
+
+
+        if self.model_dir is not None:
+            # self.restore()
+            self.restore_timestep(timestep=5526400)
+        
+
+        # Buffer
+        self.buffer = SharedReplayBuffer(
+            self.all_args,
+            self.num_agents,
+            self.envs.observation_space[0],
+            share_observation_space,
+            self.envs.action_space[0],
+        )
+    
+
+    
