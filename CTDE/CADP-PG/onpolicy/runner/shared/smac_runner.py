3eb2d5df3d1e4c6c0a76f4345a14258e3c078c39
yooodleee <accia25@naver.com>
2025-02-09 20:19:38 +0900

    run

--- CTDE/CADP-PG/onpolicy/runner/shared/smac_runner.py
+++ CTDE/CADP-PG/onpolicy/runner/shared/smac_runner.py
@@ -24,4 +24,143 @@ class SMACRunner(Runner):
         super(SMACRunner, self).__init__(config)
     
 
+    def run(self):
+        self.warmup()
+
+
+        start = time.time()
+        episodes = int(self.num_env_steps) // self.episode_length \
+                // self.n_rollout_threads
+        
+        tmp_timestep = 0
+
+        last_battles_game = np.zeros(self.n_rollout_threads, dtype=np.float32)
+        last_battles_won = np.zeros(self.n_rollout_threads, dtype=np.float32)
+
+
+        for episode in range(episodes):
+            if self.use_linear_lr_decay:
+                self.trainer.policy.lr_decay(episode, episodes)
+            
+            for step in range(self.episode_length):
+                # Sample acts
+                values, actions, action_log_probs, \
+                rnn_states, rnn_states_critic = self.collect(step)
+
+                # Obser reward and next obs
+                obs, share_obs, rewards, dones, \
+                infos, available_actions = self.envs.step(actions)
+
+                data = obs, share_obs, rewards, dones, infos, \
+                    available_actions, values, actions, action_log_probs, \
+                    rnn_states, rnn_states_critic
+                
+
+                # Insert data into buffer.
+                self.insert(data)
+            
+
+            # Compute return and update network.
+            self.compute()
+            train_infos = self.train()
+
+
+            # Post process.
+            total_num_steps = (episode + 1) * self.episode_length \
+                            * self.n_rollout_threads
+            
+            # Save model
+            if (episode % self.save_interval == 0 or episode == episodes - 1):
+                self.save()
+            
+
+            if getattr(self.all_args, 'use_CADP', False):
+                if (total_num_steps - tmp_timestep) - 500000 > 0:
+                    tmp_timestep = total_num_steps
+                    self.save_timestep(total_num_steps)
+                
+                if total_num_steps > self.all_args.cadp_breakpoint:
+                    self.trainer.use_cadp_loss = True
+            
+
+            # log info.
+            if episode % self.log_interval == 0:
+                end = time.time()
+
+                print(
+                    "\n Map {} Exp {} updates {}/{} episodes, total num timesteps {}/{}, EPS {}.\n"
+                    .format(
+                        self.all_args.map_name,
+                        self.algorithm_name,
+                        self.experiment_name,
+                        episode,
+                        episodes,
+                        total_num_steps,
+                        self.num_env_steps,
+                        int(total_num_steps / (end - start)),
+                    )
+                )
+
+                if self.env_name == "StarCraft2":
+                    battles_won = []
+                    battles_game = []
+                    incre_battles_won = []
+                    incre_battles_game = []
+
+
+                    for i, info in enumerate(infos):
+                        if 'battles_won' in info[0].keys():
+                            battles_won.append(info[0]['battles_won'])
+                            incre_battles_won.append(
+                                info[0]['battles_won'] - last_battles_won[i]
+                            )
+                        
+                        if 'battles_game' in info[0].keys():
+                            battles_game.append(info[0]['baattles_game'])
+                            incre_battles_game.append(
+                                info[0]['battles_game'] - last_battles_game[i]
+                            )
+                    
+                    incre_win_rate = np.sum(incre_battles_won) / np.sum(incre_battles_game) \
+                                    if np.sum(incre_battles_game) > 0 else 0.0
+                    
+                    print("incre win rate is {}".format(incre_win_rate))
+                    if self.use_wandb:
+                        wandb.log(
+                            {"incre_win_rate": incre_win_rate},
+                            step=total_num_steps,
+                        )
+
+                    else:
+                        self.writter.add_scalars(
+                            "incre_win_rate",
+                            {"incre_win_rate": incre_win_rate},
+                            total_num_steps,
+                        )
+                    
+                    last_battles_game = battles_game
+                    last_battles_won = battles_won
+                
+                train_infos['dead_ratio'] = 1 - self.buffer.active_masks.sum() \
+                    / reduce(lambda x, y: x * y, list(self.buffer.active_masks.shape))
+                
+                self.log_train(train_infos, total_num_steps)
+            
+            
+            # Eval.
+            if episode % self.eval_interval == 0 and self.use_eval:
+                if getattr(self.all_args, 'use_CADP', False):
+
+                    self.policy.actor.use_att_v = True
+                    self.eval(total_num_steps, "student_")
+
+                    self.policy.actor.use_att_v = False
+                    self.eval(total_num_steps, "teacher_")
+
+                    pass
+
+                else:
+                    self.eval(total_num_steps)
+    
+
     
