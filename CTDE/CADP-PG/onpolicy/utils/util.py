b92f67d7c43e367662e9de588c927434bad636b7
yooodleee <accia25@naver.com>
2025-02-10 21:41:22 +0900

    tile_images
    tile n imgs into one big PxQ img are chosen to be as close as possible, and if n is sqaure then p=q.

--- CTDE/CADP-PG/onpolicy/utils/util.py
+++ CTDE/CADP-PG/onpolicy/utils/util.py
@@ -92,3 +92,38 @@ def get_shape_from_act_space(act_space):
     return act_shape
 
 
+def tile_images(img_nhwc):
+    """
+    Tile N images into one big PxQ image
+        (P, Q) are chosen to be as close as possible, and if N
+        is square, then P = Q.
+
+    Inputs
+    ---------------
+        img_nhwc, list_or_array of images, ndim=4
+        once turned into array
+            n = batch index,
+            h = height,
+            w = width,
+            c = channel
+
+    Returns
+    ----------------
+        bitimg_HWc, ndarray with ndim=3
+    """
+    img_nhwc = np.asarray(img_nhwc)
+    N, h, w, c = img_nhwc.shape
+    
+    H = int(np.ceil(np.sqrt(N)))
+    W = int(np.ceil(float(N) / H))
+    
+    img_nhwc = np.array(
+        list(img_nhwc)
+        + [img_nhwc[0]*0 for _ in range(N, H*W)]
+    )
+    img_HWhwc = img_nhwc.reshape(H, W, h, w, c)
+    img_HWhwc = img_HWhwc.transpose(0, 2, 1, 3, 4)
+    img_Hh_Ww_c = img_HWhwc.reshape(H*h, W*w, c)
+
+
+    return img_Hh_Ww_c
