5c235391c0ae7592036ca55abcaf78c0937549bb
yooodleee <accia25@naver.com>
2025-02-10 21:07:21 +0900

    update_linear_schedule
    decrease the lr linearly.

--- CTDE/CADP-PG/onpolicy/utils/util.py
+++ CTDE/CADP-PG/onpolicy/utils/util.py
@@ -32,3 +32,19 @@ def get_grad_norm(it):
 
 
 
+def update_linear_schedule(
+        optimizer,
+        epoch,
+        total_num_epochs,
+        initial_lr):
+    
+    """
+    Decrease the learning rate linearly.
+    """
+
+    lr = initial_lr - (initial_lr * (epoch / float(total_num_epochs)))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+
