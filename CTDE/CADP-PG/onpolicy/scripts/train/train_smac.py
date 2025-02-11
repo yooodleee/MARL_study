2a9f50a1af3cfebb017ad4cd47d507fa0f91a52b
yooodleee <accia25@naver.com>
2025-02-11 10:19:54 +0900

    main

--- CTDE/CADP-PG/onpolicy/scripts/train/train_smac.py
+++ CTDE/CADP-PG/onpolicy/scripts/train/train_smac.py
@@ -95,3 +95,149 @@ def parse_args(args, parser):
     return all_args
 
 
+def main(args):
+    parser = get_config()
+    all_args = parse_args(args, parser)
+
+    if not all_args.seed_specify:
+        all_args.seed = np.random.randint(10000, 100000)
+    
+    print("seed is: ", all_args.seed)
+
+    
+    if all_args.algorithm_name == "rmappo":
+        print("u are choosing to use rmappo, we set use_recurrent_policy to be True")
+        all_args.use_recurrent_policy = True
+        all_args.use_naive_recurrent_policy = True
+    
+    elif all_args.algorithm_name == "mappo":
+        print("u are choosing to use mappo, we set use_recurrent_policy & use_naive_recurrent_policy to be False")
+        all_args.use_recurrent_policy = False
+        all_args.use_naive_recurrent_policy = False
+    
+    elif all_args.algorithm_name == "ippo":
+        print("u are choosing to use ippo, we get use_centralized_V to be False")
+        all_args.use_centralized_V = False
+    
+    else:
+        raise NotImplementedError
+    
+
+    # cuda
+    if all_args.cuda and torch.cuda.is_available():
+        print("choose to use gpu...")
+        device = torch.device("cuda:0")
+        torch.set_num_threads(all_args.n_training_threads)
+
+        if all_args.cuda_deterministic:
+            torch.backends.cudnn.benchmark = False
+            torch.backends.cudnn.deterministic = True
+    
+    else:
+        print("choose to use cpu...")
+        device = torch.device("cpu")
+        torch.get_num_threads(all_args.n_training_threads)
+    
+    run_dir = Path(
+        os.path.split(
+            os.path.dirname(os.path.abspath(__file__))
+        )[0] + "/results"
+    ) / all_args.env_name / all_args.map_name / all_args.algorithm_name / all_args.experiment_name
+
+    
+    if not run_dir.exists():
+        os.makedirs(str(run_dir))
+    
+    if all_args.use_wandb:
+        run = wandb.init(
+            config=all_args,
+            project=all_args.env_name,
+            entity=all_args.user_name,
+            notes=socket.gethostname(),
+            name=str(all_args.algorithm_name) + "_" +
+                 str(all_args.experiment_name) + 
+                 "_seed" + str(all_args.seed),
+            group=all_args.map_name,
+            dir=str(run_dir),
+            job_type="training",
+            reinit=True,
+        )
+    
+    else:
+        if not run_dir.exists():
+            curr_run = 'run1'
+        
+        else:
+            exst_run_nums = [
+                int(str(folder.name).split('run')[1])
+                for folder in run_dir.iterdir()
+                if str(folder.name).startswith('run')
+            ]
+
+            if len(exst_run_nums) == 0:
+                curr_run = 'run1'
+            
+            else:
+                curr_run = 'run%i' % (max(exst_run_nums) + 1)
+            
+        run_dir = run_dir / curr_run
+        if not run_dir.exists():
+            os.makedirs(str(run_dir))
+        
+    setproctitle.setproctitle(
+        str(all_args.algorithm_name) + "-"
+        + str(all_args.env_name) + "-"
+        + str(all_args.experiment_name) + "@"
+        + str(all_args.user_name)
+    )
+
+    
+    # seed
+    torch.manual_seed(all_args.seed)
+    torch.cuda.manual_seed_all(all_args.seed)
+    np.random.seed(all_args.seed)
+
+
+    # env
+    envs = make_train_env(all_args)
+    eval_envs = make_eval_env(all_args) if all_args.use_eval else None
+    num_agents = get_map_params(all_args.map_name)["n_agents"]
+
+    config = {
+        "all_args": all_args,
+        "envs": envs,
+        "eval_envs": eval_envs,
+        "num_agents": num_agents,
+        "device": device,
+        "run_dir": run_dir,
+    }
+
+
+    # run experiments.
+    if all_args.share_policy:
+        from onpolicy.runner.shared.smac_runner import SMACRunner as Runner
+    
+    else:
+        from onpolicy.runner.separated.smac_runner import SMACRunner as Runner
+    
+    runner = Runner(config)
+    runner.run()
+
+
+    # post process
+    envs.close()
+    if all_args.use_eval and eval_envs is not envs:
+        eval_envs.close()
+    
+    if all_args.use_wandb:
+        run.finish()
+    
+    else:
+        runner.writter.export_scalars_to_json(str(runner.log_dir + '/summary.json'))
+        runner.writter.close()
+
+
+
+if __name__ == "__main__":
+    
+    main(sys.argv[1:])
