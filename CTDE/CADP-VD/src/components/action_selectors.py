c97ece6c58888e2902a0453e1c2ebdacd86035d3
yooodleee <accia25@naver.com>
2025-02-11 11:43:22 +0900

    select_action

--- CTDE/CADP-VD/src/components/action_selectors.py
+++ CTDE/CADP-VD/src/components/action_selectors.py
@@ -50,4 +50,34 @@ class EpsilonGreedyActionSelector():
         self.epsilon = self.schedule.eval(0)
     
 
-    
+    def select_action(
+            self,
+            agent_inputs,
+            avail_actions,
+            t_env,
+            test_mode=False):
+        
+        # Assuming agent_inputs is a batch of Q-values for each agent bav
+        self.epsilon = self.schedule.eval(t_env)
+
+        if test_mode:
+            # Greedy action selection only
+            self.epsilon = 0.0
+        
+
+        # Mask actions that are excluded from selection
+        masked_q_values = agent_inputs.clone()
+        masked_q_values[avail_actions==0.0] = -float("inf") # should never be selected!
+
+
+        random_numbers = torch.rand_like(agent_inputs[:, :, 0])
+        pick_random = (random_numbers < self.epsilon).long()
+        random_actions = Categorical(avail_actions.float()).sample().long()
+
+
+        picked_actions = pick_random * random_actions + (1 - pick_random) * masked_q_values.max(dim=2)[1]
+
+        return picked_actions
+
+
+REGISTRY["epsilon_greedy"] = EpsilonGreedyActionSelector
