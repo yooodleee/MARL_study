ac1e06dc0d8f1f9d6503495020827320d5aa089f
yooodleee <accia25@naver.com>
2025-02-11 11:35:35 +0900

    select_action
    test mode for picked actions

--- CTDE/CADP-VD/src/components/action_selectors.py
+++ CTDE/CADP-VD/src/components/action_selectors.py
@@ -17,4 +17,28 @@ class MultinomialActionSelector():
         self.test_greedy = getattr(args, "test_greedy", True)
     
 
+    def select_action(
+            self,
+            agent_inputs,
+            avail_actions,
+            t_env,
+            test_mode=False):
+        
+        masked_policies = agent_inputs.clone()
+        masked_policies[avail_actions==0.0] = 0.0
+
+        self.epsilon = self.schedule.eval(t_env)
+
+        if test_mode and self.test_greedy:
+            picked_actions = masked_policies.max(dim=2)[1]
+        
+        else:
+            picked_actions = Categorical(masked_policies).sample().long()
+        
+        return picked_actions
     
+
+REGISTRY["multinomial"] = MultinomialActionSelector
+
+
+
