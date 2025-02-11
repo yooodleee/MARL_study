1e7159053134c79c133a7aafb13f6bb841365d99
yooodleee <accia25@naver.com>
2025-02-11 09:48:49 +0900

    make_eval_env

--- CTDE/CADP-PG/onpolicy/scripts/train/train_smac.py
+++ CTDE/CADP-PG/onpolicy/scripts/train/train_smac.py
@@ -50,3 +50,29 @@ def make_train_env(all_args):
 
 
 
+def make_eval_env(all_args):
+
+    def get_env_fn(rank):
+
+        def init_env():
+            if all_args.env_name == "StarCraft2":
+                env = StarCraft2Env(all_args)
+            
+            else:
+                print("Can not support the " + all_args.env_name + "environment.")
+                raise NotImplementedError
+            
+            env.seed(all_args.seed * 50000 + rank * 10000)
+
+            return env
+        
+        return init_env
+    
+    if all_args.n_eval_rollout_threads == 1:
+        return ShareDummyVecEnv([get_env_fn(0)])
+    
+    else:
+        return ShareSubprocVecEnv([get_env_fn(i) for i in range(all_args.n_evel_rollout_threads)])
+
+
+
