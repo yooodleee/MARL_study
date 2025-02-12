6a03d4c48465d070b67bfe5ab52fea306ef7638b
yooodleee <accia25@naver.com>
2025-02-12 16:29:48 +0900

    init
    add DecayThenFlatSchedule

---
+++ CTDE/CADP-VD/src/components/epsilon_schedules.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+
+class DecayThenFlatSchedule():
+
+    def __init__(
+            self,
+            start,
+            finish,
+            time_length,
+            decay="exp"):
+        
+        self.start = start
+        self.finish = finish
+        self.time_length = time_length
+        self.delta = (self.start - self.finish) / self.time_length
+        self.decay = decay
+
+        if self.decay in ["exp"]:
+            self.exp_scailing = (-1) * self.time_length / np.log(self.finish) if self.finish > 0 else 1
+    
+
+    
