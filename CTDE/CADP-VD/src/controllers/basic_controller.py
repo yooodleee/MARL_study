ccd4aabf90659fa466157a3af37532e8dd176a08
yooodleee <accia25@naver.com>
2025-02-12 17:24:10 +0900

    select_actions
    only select acts for the selected batch elements in bs.

--- CTDE/CADP-VD/src/controllers/basic_controller.py
+++ CTDE/CADP-VD/src/controllers/basic_controller.py
@@ -29,4 +29,26 @@ class BasicMAC:
         self.hidden_states = None
     
 
+    def select_actions(
+            self,
+            ep_batch,
+            t_ep,
+            t_env,
+            bs=slice(None),
+            test_mode=False):
+        
+        """
+        Only select acts for the selected batch elements in bs.
+        """
+
+        avail_actions = ep_batch["avail_actions"][:, t_ep]
+        agent_outputs = self.forward(ep_batch, t_ep, test_mode=test_mode)
+        chosen_actions = self.action_selector.select_action(agent_outputs[bs],
+                                                            avail_actions[bs],
+                                                            t_env,
+                                                            test_mode=test_mode)
+        
+        return chosen_actions
+    
+
     
