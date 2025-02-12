96dc5f4baa9a416d1f71ca54e809b70623e459f7
yooodleee <accia25@naver.com>
2025-02-12 17:33:34 +0900

    forward

--- CTDE/CADP-VD/src/controllers/basic_controller.py
+++ CTDE/CADP-VD/src/controllers/basic_controller.py
@@ -51,4 +51,64 @@ class BasicMAC:
         return chosen_actions
     
 
+    def forward(
+            self,
+            ep_batch,
+            t,
+            test_mode=False):
+        
+        agent_inputs = self._build_inputs(ep_batch, t)
+        avail_actions = ep_batch["avail_actions"][:, t]
+        agent_outs, self.hidden_states = self.agent(agent_inputs, self.hidden_states)
+
+
+        # softmax the agent outputs if they're policy logits.
+        if self.agent_output_type == "pi_logits":
+            if getattr(
+                self.args,
+                "mask_before_softmax",
+                True
+            ):
+                # make the logits for unavailable acts very negative to minimise 
+                # their affect on the softmax.
+                reshaped_avail_actions = avail_actions.reshape(ep_batch.batch_size * self.n_agents, -1)
+                agent_outs[reshaped_avail_actions == 0] = -1e10
+            
+            
+            agent_outs = torch.nn.functional.softmax(agent_outs, dim=-1)
+            if not test_mode:
+                # Epsilon floor
+                epsilon_action_num = agent_outs.size(-1)
+
+                if getattr(
+                    self.args,
+                    "mask_before_softmax",
+                    True
+                ):
+                    # with prob eps, pick an available act uniformly
+                    epsilon_action_num = reshaped_avail_actions.sum(dim=-1, keepdim=True).float()
+                
+                
+                agent_outs = (
+                    (1 - self.action_selector.epsilon) 
+                    * agent_outs
+                    + torch.ones_like(agent_outs)
+                    * self.action_selector.epsilon 
+                    / epsilon_action_num
+                )
+
+                if getattr(
+                    self.args,
+                    "mask_before_softmax",
+                    True
+                ):
+                    # zero out the unavailable acts
+                    agent_outs[reshaped_avail_actions == 0] = 0.0
+        
+
+        return agent_outs.view(
+            ep_batch.batch_size, self.n_agents, -1
+        )
+    
+
     
