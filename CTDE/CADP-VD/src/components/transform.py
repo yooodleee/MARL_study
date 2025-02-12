579f7ccebcaa53736e6d77f3d5eef4387e0ae416
yooodleee <accia25@naver.com>
2025-02-12 16:36:06 +0900

    overrides Transform

--- CTDE/CADP-VD/src/components/transform.py
+++ CTDE/CADP-VD/src/components/transform.py
@@ -20,4 +20,13 @@ class OneHot(Transform):
         self.out_dim = out_dim
     
 
+    def transform(self, tensor):
+        y_onehot = tensor.new(*tensor.shape[:-1], self.out_dim).zero_()
+        y_onehot.scatter_(-1, tensor.long(), 1)
+
+        return y_onehot.float()
     
+
+    def infer_output_info(self, vshape_in, dtype_in):
+        
+        return (self.out_dim,), torch.float32
