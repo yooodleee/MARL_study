5fb04e3bf24d21b72340933dd5f3b4e7fabd850b
yooodleee <accia25@naver.com>
2025-02-15 18:40:50 +0900

    init
    add StarCraft2Renderer

--- CTDE/CADP-VD/src/envs/starcraft2/render.py
+++ CTDE/CADP-VD/src/envs/starcraft2/render.py
@@ -5,6 +5,7 @@ import subprocess
 import platform
 from absl import logging
 import math
+import time
 import collections
 import os
 import pygame
@@ -45,3 +46,61 @@ def _get_desktop_size():
 
 
 
+class StarCraft2Renderer:
+
+    def __init__(self, env, mode):
+        os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+
+        self.env = env
+        self.mode = mode
+        self.obs = None
+        self._window_scale = 0.75
+        self.game_info = game_info = self.env._controller.game_info()
+        self.static_data = self.env._controller.data()
+
+        self._obs_queue = queue.Queue()
+        self._game_times = collections.deque(maxlen=100)    # Avg FPS over 100 frames.
+        self._render_times = collections.deque(maxlen=100)
+        self._last_time = time.time()
+        self._ast_game_loop = 0
+        self._name_lengths = {}
+
+        self._map_size = point.Point.build(game_info.start_raw.map_size)
+        self._playable = point.Rect(
+            point.Point.build(game_info.start_raw.playable_area.p0),
+            point.Point.build(game_info.start_raw.playable_area.p1),
+        )
+
+        window_size_px = point.Point(self.env.window_size[0], self.env.window_size[1])
+        window_size_px = self._map_size.scale_max_size(window_size_px * self._window_scale).ceil()
+        self._scale = window_size_px.y // 32
+
+        self.display = pygame.Surface(window_size_px)
+
+        if mode == "human":
+            self.display = pygame.display.set_mode(window_size_px, 0, 32)
+            pygame.display.init()
+            pygame.display.set_caption("Starcraft Viewer")
+        
+        pygame.font.init()
+        self._world_to_world_t1 = transform.Linear(point.Point(1, -1), point.Point(0, self._map_size.y))
+        self._world_t1_to_screen = transform.Linear(scale=window_size_px / 32)
+        self.screen_transform = transform.Chain(self._world_to_world_t1, self._world_t1_to_screen)
+
+        surf_loc = point.Rect(point.origin, window_size_px)
+        sub_surf = self.display.subsurface(pygame.Rect(surf_loc.t1, surf_loc.size))
+        
+        self._surf = _Surface(
+            sub_surf,
+            None,
+            surf_loc,
+            self.screen_transform,
+            None,
+            self.draw_screen,
+        )
+
+        self._font_small = pygame.font.Font(None, int(self._scale * 0.5))
+        self._font_large = pygame.font.Font(None, self._scale)
+
+    
+    
