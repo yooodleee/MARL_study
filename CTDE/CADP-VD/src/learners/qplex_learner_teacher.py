b38c3b369956db1126388d8da7c30be020d32d8a
yooodleee <accia25@naver.com>
2025-02-17 15:27:03 +0900

    init
    add DMAQ_qattenLearner

--- CTDE/CADP-VD/src/learners/qplex_learner_teacher.py
+++ CTDE/CADP-VD/src/learners/qplex_learner_teacher.py
@@ -8,7 +8,7 @@ from torch.optim import RMSprop, Adam
 
 
 from components.episode_buffer import EpisodeBatch
-from modules.mixers.qplex import DMAQ_Q_attenMixer
+from modules.mixers.qplex import DMAQ_QattenMixer
 from utils.th_utils import get_params_size
 
 
@@ -21,3 +21,47 @@ def entropy(x, dim=-1):
 
 
 
+class DMAQ_qattenLearner:
+
+    def __init__(
+            self,
+            mac,
+            scheme,
+            logger,
+            args
+    ):
+        self.args = args
+        self.mac = mac
+        self.logger = logger
+
+        self.params = list(mac.parameters())
+
+        self.last_target_update_episode = 0
+
+        self.mixer = None
+        if args.mixer is not None:
+            if args.mixer == "dmaq_qatten":
+                self.mixer = DMAQ_QattenMixer
+            else:
+                raise ValueError("Mixer {} not recognized.".format(args.mixer))
+
+            self.params += list(self.mixer.parameters())
+            self.target_mixer = copy.deepcopy(self.mixer)
+        
+        if self.args.optimizer == "adam":
+            self.optimizer = Adam(params=self.params, lr=args.lr)
+        else:
+            self.optimizer = RMSprop(
+                params=self.params, lr=args.lr, alpha=args.optimi_alpha, eps=args.optim_eps
+            )
+        
+
+        # a little wasteful to deepcopy (e.g. duplicates act selector), but shoule work for any MAC
+        self.target_mac = copy.deepcopy(mac)
+
+        self.log_stats_t = -self.args.learner_log_interval - 1
+
+        self.n_actions = self.args.n_actions
+    
+
+    
