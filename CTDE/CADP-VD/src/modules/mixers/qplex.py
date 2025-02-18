2b53ca6118e1ef9f7b7b3d82c6480962f601765f
yooodleee <accia25@naver.com>
2025-02-18 16:47:42 +0900

    init
    add DMAQ_QattenMixer

---
+++ CTDE/CADP-VD/src/modules/mixers/qplex.py
@@ -0,0 +1,25 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+
+class DMAQ_QattenMixer(nn.Module):
+
+    def __init__(self, args):
+        super(DMAQ_QattenMixer, self).__init__()
+
+        self.args = args
+        self.n_agents = args.n_agents
+        self.n_actions = args.n_actions
+        self.state_dim = int(np.prod(args.state_shape))
+        self.action_dim = args.n_agents * self.n_actions
+        self.state_action_dim = self.state_dim + self.action_dim + 1
+
+        self.attention_weight = Qatten_Weight(args)
+        self.si_weight = DMAQ_SI_Weight(args)
+    
+
+    
