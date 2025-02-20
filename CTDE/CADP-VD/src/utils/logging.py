53f732410fdbb6db3adc8e60fe45c0d0984a347a
yooodleee <accia25@naver.com>
2025-02-20 17:35:03 +0900

    get_logger

--- CTDE/CADP-VD/src/utils/logging.py
+++ CTDE/CADP-VD/src/utils/logging.py
@@ -65,3 +65,17 @@ class Logger:
 
 
 
+# Set up a custom logger
+def get_logger():
+    logger = logging.getLogger()
+    logger.handlers = []
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '[%(levelname)s %(asctime)s] %(name)s %(message)s', '%H:%M:%S'
+    )
+
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.setLevel('DEBUG')
+
+    return logger
