90c23e4944dbe44901699ec4194ff794020c07eb
yooodleee <accia25@naver.com>
2025-02-20 17:14:58 +0900

    add CloudpickleWrapper

--- CTDE/CADP-VD/src/runners/parallel_runner.py
+++ CTDE/CADP-VD/src/runners/parallel_runner.py
@@ -352,3 +352,21 @@ def env_worker(remote, env_fn):
 
 
 
+class CloudpickleWrapper():
+    """
+    Uses cloudpickle to serialize contents (otherwise multiprocessing tries 
+    to use pickle).
+    """
+
+    def __init__(self, x):
+        self.x = x
+    
+    
+    def __getstate__(self):
+        import cloudpickle
+        return cloudpickle.dumps(self.x)
+    
+
+    def __setstate__(self, ob):
+        import pickle
+        self.x = pickle.loads(ob)
