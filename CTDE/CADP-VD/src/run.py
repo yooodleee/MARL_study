7ddc17e3c05dffe73fc2d8ef03aac0b343ac87a7
yooodleee <accia25@naver.com>
2025-02-20 18:45:40 +0900

    args_sanity_check
    set CUDA flags

--- CTDE/CADP-VD/src/run.py
+++ CTDE/CADP-VD/src/run.py
@@ -267,3 +267,21 @@ def run_sequential(args, logger):
 
 
 
+def args_sanity_check(config, _log):
+
+    # set CUDA flags
+    # config["use_cuda"] = True # Use cuda whenever possible!
+    if config["use_cuda"] and not torch.cuda.is_available():
+        config["use_cuda"] = False
+        _log.warning(
+            "CUDA flag use_cuda was swithced OFF automatically because no CUDA devices are available!"
+        )
+
+    if config["test_nepisode"] < config["batch_size_run"]:
+        config["test_nepisode"] = config["batch_size_run"]
+    
+    else:
+        config["test_nepisode"] = (config["test_nepisode"] // config["batch_size_run"]) * config["batch_size_run"]
+    
+    
+    return config
